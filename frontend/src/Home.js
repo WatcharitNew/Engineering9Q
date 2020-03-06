0b8d4fbbe274ade066e6e853873e717b73357a25
Watcharit Tuntayakul <watcharit.new@gmail.com>
2020-03-06 23:37:12 +0700

    Draft Home Page

---
+++ frontend/src/Home.js
@@ -0,0 +1,56 @@
+import React,{Component} from 'react';
+import './Home.css';
+import NavBar from './NavBar';
+import { Container, Row, Col, Form, Button } from 'react-bootstrap';
+
+
+class Home extends Component{
+    constructor(props){
+        super(props);
+        this.state={
+        };
+    }
+
+    render(){
+        return(
+            <div className="main-bg">
+                <NavBar />
+                <Container id="home-box">
+                    <Row>
+                        <Col className="text-center">
+                            <h1 className="text-white">แบบประเมิณ 9Q</h1>
+                        </Col>
+                    </Row>
+                    <Row className="mt-5 mb-1">
+                        <Col xs={2}></Col>
+                        <Col xs={3} className="text-right">
+                            <Form.Label><h4 className="text-white">ชื่อ-นามสกุล</h4></Form.Label>
+                        </Col >
+                        <Col xs={5} className="text-left">
+                            <Form.Control type="text" placeholder="ชื่อ นามสกุล" required/>
+                        </Col>
+                    </Row>
+                    <Row className="mt-1 mb-5">
+                        <Col xs={2}></Col>
+                        <Col xs={3} className="text-right">
+                            <Form.Label><h4 className="text-white">รหัสนิสิต</h4></Form.Label>
+                        </Col >
+                        <Col xs={3} className="text-left">
+                            <Form.Control type="text" placeholder="xxxxxxxx21" required/>
+                        </Col>
+                        <Col xs={2}></Col>
+                    </Row>
+                    <Row>
+                        <Col className="text-center" >
+                            <Button id="begin-button" type="submit">
+                                เริ่มทำแบบทดสอบ
+                            </Button>
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
+        );
+    }
+}
+  
+export default Home;
---
+++ frontend/src/NavBar.js
@@ -0,0 +1,25 @@
+import React,{Component} from 'react';
+import './NavBar.css';
+import logo from './material/EngineeringLogo.png';
+import {Navbar} from 'react-bootstrap';
+
+class NavBar extends Component{
+    constructor(props){
+        super(props);
+        this.state={
+        };
+    }
+
+    render(){
+        var logoBrand=(
+            <img src={logo} alt="logo" id="logo"/>
+        );
+        return(
+            <Navbar expand="lg" id="navbar" sticky="top" className="shadow">
+                {logoBrand}
+            </Navbar>
+        );
+    }
+}
+  
+export default NavBar;
