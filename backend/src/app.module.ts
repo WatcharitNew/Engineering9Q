d8d10528a0bbd704d5f64dddf57cebaf2e98a1e0
dolwijit13 <dol51890@gmail.com>
2020-03-12 15:46:50 +0700

    add serch by filter

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,21 +5,22 @@ import { UsersModule } from './users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UsersModule,
+  imports: [
+    UsersModule,
     TypeOrmModule.forRootAsync({
       useFactory: () => ({
-          type: 'mysql',
-          host: process.env.MYSQL_URL,
-          port: 3306,
-          username: process.env.MYSQL_USER,
-          password: process.env.MYSQL_PASSWORD,
-          database: process.env.MYSQL_DB,
-          entities: [__dirname + '/**/*.entity{.ts,.js}'],
-          synchronize: true,
-          charset: 'utf8mb4_general_ci',
-      })
-    })
-    ],
+        type: 'mysql',
+        host: process.env.MYSQL_URL,
+        port: 3306,
+        username: process.env.MYSQL_USER,
+        password: process.env.MYSQL_PASSWORD,
+        database: process.env.MYSQL_DB,
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: true,
+        charset: 'utf8mb4_general_ci',
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- backend/src/entities/user.entity.ts
+++ backend/src/entities/user.entity.ts
@@ -1,50 +1,50 @@
-import { Entity, Column, PrimaryColumn } from "typeorm";
+import { Entity, Column, PrimaryColumn } from 'typeorm';
 
 export enum Major {
-    cp = 'cp',
-    chem = 'chem',
-    ne = 'ne',
-    me = 'me',
-    ee = 'ee',
-    ce = 'ce',
-    metal = 'metal',
-    sv = 'sv',
-    env = 'env',
-    mining = 'mining',
-    water = 'water',
-    ie = 'ie',
-    adme = 'adme',
-    aero = 'aero',
-    ice = 'ice',
-    nano = 'nano',
-    robotic = 'robotic',
-    bme = 'bme',
-    other = 'other',
+  cp = 'cp',
+  chem = 'chem',
+  ne = 'ne',
+  me = 'me',
+  ee = 'ee',
+  ce = 'ce',
+  metal = 'metal',
+  sv = 'sv',
+  env = 'env',
+  mining = 'mining',
+  water = 'water',
+  ie = 'ie',
+  adme = 'adme',
+  aero = 'aero',
+  ice = 'ice',
+  nano = 'nano',
+  robotic = 'robotic',
+  bme = 'bme',
+  other = 'other',
 }
 
 @Entity()
 export class User {
-    @PrimaryColumn('varchar', { length: 100 })
-    userId: string;
+  @PrimaryColumn('varchar', { length: 100 })
+  userId: string;
 
-    @Column('integer')
-    sumScore: number;
+  @Column('integer')
+  sumScore: number;
 
-    @Column('varchar', { length: 100 })
-    name: string;
+  @Column('varchar', { length: 100 })
+  name: string;
 
-    @Column('timestamp', { default: () => 'CURRENT_TIMESTAMP' })
-    createdTime: Date;
+  @Column('timestamp', { default: () => 'CURRENT_TIMESTAMP' })
+  createdTime: Date;
 
-    @Column('simple-array')
-    scores: number[];
+  @Column('simple-array')
+  scores: number[];
 
-    @Column('varchar', { length: 100 })
-    major: Major;
+  @Column('varchar', { length: 100 })
+  major: Major;
 
-    @Column('text')
-    help: string;
+  @Column('text')
+  help: string;
 
-    @Column('text', {nullable : true})
-    comment: string;
+  @Column('text', { nullable: true })
+  comment: string;
 }
