88e8aabd60046d1885b3b0475238feea68b99c4d
Watcharit Tuntayakul <watcharit.new@gmail.com>
2020-05-04 09:12:39 +0700

    add secure

--- frontend/src/utilities/AdminRoute.js
+++ frontend/src/utilities/AdminRoute.js
@@ -4,13 +4,13 @@ import SessionStorageService from "../SessionStorageService";
 var CryptoJS = require("crypto-js");
 
 function AdminRoute({ component: Component, ...rest }) {
-    var bytes  = CryptoJS.AES.decrypt(SessionStorageService.getToken(), 'saroj');
+    var bytes  = CryptoJS.AES.decrypt(SessionStorageService.getToken(), process.env.REACT_APP_ADMIN_PASSWORD);
     var originalText = bytes.toString(CryptoJS.enc.Utf8);
     return (
         <Route
             {...rest}
             render={props =>
-                originalText === "admin" ? (
+                originalText === process.env.REACT_APP_ADMIN_USERNAME ? (
                     <Component {...props} />
                 ) : (
                         <Redirect to="/" />
